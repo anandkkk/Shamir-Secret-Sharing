90a5ebd1649efdebe36df544dd5c14b0096aa940
anandkkk <anandkk1989@gmail.com>
2016-06-25 15:39:46 +0530

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -3,19 +3,11 @@ import edu.biu.scapi.primitives.dlog.CryptoPpDlogGF;
 
 public class Polynomial extends CryptoPpDlogGF{
 
-/*
-
-x[], y[] are points arrays
-n : # of points
-x_position : x coordinate at which we want to find y
-
- */
-	int x[],y[],w[],v[];
-	int n,x_position;
-	//CryptoPpDlogGF ring = new CryptoPpDlogGF();
-	private native void PrepareBulkPolynomialInterpolation(long ring,int w[], int x[], int n);
-	private native void PrepareBulkPolynomialInterpolationAt(long ring, int v[], int x_position, int x[], int w[], int n);
-	private native int BulkPolynomialInterpolateAt(long ring,int y[], int v[], int n);
+	//private int x[],y[];
+	//private int n,x_position;
+	private native int[] PrepareBulkPolynomialInterpolation(long ring,int x[], int n);
+	private native int[] PrepareBulkPolynomialInterpolationAt(long ring,int x_position,int x[],int w[],int n);
+	private native int BulkPolynomialInterpolateAt(long ring,int y[],int v[],int n);
 	
 
 	static {
@@ -28,21 +20,19 @@ x_position : x coordinate at which we want to find y
 		super();
 	}
 
-	public int doInterpolation(int X[],int Y[], int N, int X_Position)
+	public int doInterpolation(int x[],int y[], int n, int x_position)
 	{
 		
-		
-		n=N;
-		x_position = X_Position;
-		x=new int[n];		
-		x=X;
-		y=new int[n];		
-		y=Y;
-		w=new int[n];		
-		v=new int[n];		
-
-		PrepareBulkPolynomialInterpolation(pointerToField,w,x,n);
-		PrepareBulkPolynomialInterpolationAt(pointerToField,v,x_position,x,w,n);
+		System.out.println("In Interpolation");
+	
+		int[] w = PrepareBulkPolynomialInterpolation(pointerToField,x,n);
+		for(int i=0;i<n;i++){
+			System.out.println("w["+i+"] = "+w[i]);
+		}
+		int[] v = PrepareBulkPolynomialInterpolationAt(pointerToField,x_position,x,w,n);
+		for(int i=0;i<n;i++){
+			System.out.println("v["+i+"] = "+v[i]);
+		}
 		return BulkPolynomialInterpolateAt(pointerToField,y,v,n);
 	}
 
